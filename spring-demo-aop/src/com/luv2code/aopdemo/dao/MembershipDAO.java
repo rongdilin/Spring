628d8448d8d0e7140b0c52e47b6224c178d69ec3
Rongdi <rongdili@buffalo.edu>
2017-07-04 15:48:29 -0700

    Add files via upload

---
+++ spring-demo-aop/src/com/luv2code/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,18 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipDAO {
+
+	public void addAccount(){
+		
+		System.out.println(getClass() + ": DOING STUFF: ADDING A MEMBERSHIP ACCOUNT");
+	}
+	
+	public void goToSleep(){
+		
+		System.out.println(getClass() + ": I'm going to sleep now...");
+
+	}
+}
